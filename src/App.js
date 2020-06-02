3cf907d766a50cbdc493a6af70ec183c32dc5b20
吳彥廷 <roy@wuyantingde-MacBook-Pro.local>
2020-06-02 13:17:20 +0800

    creaing a Header component and adding it into the whole application

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 // Link is a component for connecting other pages with path
 
 import './App.css';
 
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shoppage/ShopPage';
+import Header from './components/Header/Header'
 
 
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop/hats' component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }

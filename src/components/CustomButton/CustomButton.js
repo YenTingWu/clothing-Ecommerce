d28dedbaa3babb08ea69fca076704f522a9540a7
吳彥廷 <roy@wuyantingde-MacBook-Pro.local>
2020-06-03 20:02:13 +0800

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './CustomButton.scss';
 
-const CustomButton = ({ children, isGoogleSingIn, ...otherProps }) => (
-    <button className={`${isGoogleSingIn ? 'google-sign-in' : ''} custom-button`}  {...otherProps}>
+const CustomButton = ({ children, isGoogleSingIn, inverted, ...otherProps }) => (
+    <button className={`
+        ${inverted ? 'inverted' : ''} 
+        ${isGoogleSingIn ? 'google-sign-in' : ''} 
+        custom-button
+        `}  
+        {...otherProps}>
         {children}
     </button>
 );
